4cbcc7a34c7f2d74ac16bd9f15336552aa810c21
Judy Ladella <batman@ucsc-guest-169-233-112-226.ucsc.edu>
2024-02-03 15:02:58 -0800

    modified files

--- volunteering_opportunities/lib/main.dart
+++ volunteering_opportunities/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(title: 'Login'),
+      home: const Login(title: 'Title'),
     );
   }
 }
