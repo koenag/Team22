07ac3796ff8f96fb42571607a301d75c5a169631
koenag <104934193+koenag@users.noreply.github.com>
2024-02-03 17:24:31 -0800

    I added more text boxes and an images file. I also added assets to the pubspec.yaml

--- volunteering_opportunities/lib/description_page.dart
+++ volunteering_opportunities/lib/description_page.dart
@@ -6,35 +6,62 @@ class DescriptionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children:[
-              Text(
-                'Title of Opportunity',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 24,
-                ),
-              ),
-              
-              const SizedBox(height:25),
+      backgroundColor: Colors.grey[200],
 
-              Container(
-                height: 300,
-                color: Colors.grey[300],
-                child: Text(
-                  'This is the area where an organization can put a more detailed description of what the volunteering opportunity entails in order for students to make a more informed decision about the time commitment.',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+      body: Center(
+        child: Column(
+          children:[
+            Text(
+              'Title of Opportunity',
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 24,
+              ),
+            ),
+      
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(12.0)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    'This is the area where an organization can put a more detailed description of what the volunteering opportunity entails in order for students to make a more informed decision about the time commitment.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
               ),
-            ]
-          ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: 40,
+                color: Colors.amber,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Contact Information',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    )
+                  ),
+                ),
+              ),
+            ),
+          
+
+          ]
         ),
       )
     );
