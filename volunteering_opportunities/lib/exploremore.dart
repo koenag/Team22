2efef53f02b5751b14545953cc0e30fc59cc902a
ananya <asetty1@ucsc.edu>
2024-02-05 21:42:24 -0800

    Added scrolling and duplicate widgets plus a header that does not work

--- volunteering_opportunities/lib/exploremore.dart
+++ volunteering_opportunities/lib/exploremore.dart
@@ -1,86 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ExploreMore extends StatelessWidget {
+  const ExploreMore({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Volunteering Fun'),
+          title: const Text('Slug Impact', style: TextStyle(color: Colors.lightBlue, fontStyle: FontStyle.italic),),
         ),
-        body: Center(
-          child: buildListItem(context),
-        ),
-      ),
-    );
-  }
-
-  Widget buildListItem(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(12),
-      elevation: 4,
-      color: Color.fromRGBO(32, 50, 76, .9),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-        child: Row(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text("Name of Opportunity", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
-                SizedBox(height: 4),
-                Text("Off Campus/On Campus", style: TextStyle(color: Colors.white70)),
-                Text("Remote/In-Person/Hybrid", style: TextStyle(color: Colors.white70)),
-                Text("Length of Volunteering", style: TextStyle(color: Colors.white70)),
-              ],
+        body: Column(
+          children: [
+            SizedBox(
+              height: 150, // Adjust the height as needed
+              child: Image.asset(
+                'volunteering_opportunities/lib/headers/image-remove-preview(1).png', // Replace with the path to your image asset
+                fit: BoxFit.cover, // Adjust the fit as needed
+              ),
             ),
-            Spacer(),
-            IconButton(
-              icon: Icon(Icons.favorite, color: Colors.red), // Heart-shaped icon
-              onPressed: () {
-                // Add your onPressed functionality here
-                print("Added to Favorites!");
-              },
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return buildListItem(context);
+                },
+              ),
             ),
           ],
         ),
       ),
     );
   }
-}
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
-            ),
+  Widget buildListItem(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Add your onTap functionality here
+        print("Card clicked!");
+      },
+    child : Card (
+      margin: EdgeInsets.all(12),
+      elevation: 4,
+      color: Color.fromRGBO(32, 50, 76, .9),
+      child: Container(
+        decoration: BoxDecoration(
+          //color : Colors.blue,
+          //borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60)),
+          border: Border(
+            right: BorderSide(color: Colors.blue, width: 5.0),
+            bottom: BorderSide(color: Colors.blue, width: 5.0),),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+          child: Row(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      // Add your onTap functionality here
+                      print("Name of Opportunity clicked!");
+                    },
+                    child: Text(
+                      "Name of Opportunity",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text("Off Campus/On Campus", style: TextStyle(color: Colors.white70)),
+                  Text("Remote/In-Person/Hybrid", style: TextStyle(color: Colors.white70)),
+                  Text("Length of Volunteering", style: TextStyle(color: Colors.white70)),
+                ],
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.favorite, color: Colors.grey), // Heart-shaped icon
+                onPressed: () {
+                  // Add your onPressed functionality here
+                  icon: Icon(Icons.favorite, color: Colors.red);
+                  print("Added to Favorites!");
+                },
+              ),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
-            ),
-          ),
-        ),
-      ],
+      )
+      ),
     );
   }
 }
